3499590db3aeda35f1135ecb8ca3db3fc33fa479
Steven Basher <sbasher314@gmail.com>
2021-05-05 16:42:59 -0400

    Complete basic API and react template

--- client/src/components/Gallery.jsx
+++ client/src/components/Gallery.jsx
@@ -4,7 +4,17 @@ import React from 'react';
 class Gallery extends React.Component {
   render() {
     return (
-      <div>Placeholder</div>
+      <div class="imageBlock">
+        <img src="https://placeimg.com/360/480/tech" />
+        <div class="imageCaption">
+          Click image to open expanded view
+        </div>
+        <ul class="altImages">
+          <li class="selected"><img src="https://placeimg.com/360/480/tech" /></li>
+          <li><img src="https://placeimg.com/360/480/animals" /></li>
+          <li><img src="https://placeimg.com/480/360/arch" /></li>
+        </ul>
+      </div>
     );
   }
 }
--- server/index.js
+++ server/index.js
@@ -3,8 +3,30 @@ const app = express();
 const port = 1166;
 const db = require('../database/index.js');
 
+app.use(express.static(__dirname + '/../client/dist'));
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '../../client/dist/index.html');
+  res.sendFile(__dirname + '/../client/dist/index.html');
+});
+
+app.get('/images/:productId', (req, res) => {
+  const productId = req.params.productId;
+  db.models.ProductImages.findOne({productId}, (err, product) => {
+    if (err) {
+      res.status(404); //wrong code but whatever
+    } else {
+      if (product !== null) {
+        res.json(product);
+      } else {
+        res
+          .status(404)
+          .json({
+            productId,
+            images: [],
+          });
+      }
+    }
+  });
+  //res.json({productId});
 });
 
 app.listen(port, () => {
