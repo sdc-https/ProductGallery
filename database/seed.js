efa2ad7255e80cf91aa0bbf872f0fa9666b51614
Steven Basher <sbasher314@gmail.com>
2021-05-10 12:04:03 -0400

    Adjusted seed to account for more images / sizes

--- database/seed.js
+++ database/seed.js
@@ -1,23 +1,32 @@
 const db = require('./index.js');
 
+let products = 100;
+let count = 0;
 const generateImages = (size = 0) => {
   const url = 'https://placeimg.com/';
-  const sizes = [360, 480];
-  const tags = ['tech', 'animals', 'arch', 'people'];
+  const sizes = [960, 1280, 1500];
+  const tags = ['tech', 'animals', 'arch', 'people', 'nature'];
   const images = [];
   for (let i = 0; i < size; i++) {
     const width = sizes[Math.round(Math.random() * (sizes.length - 1))];
     const height = sizes[Math.round(Math.random() * (sizes.length - 1))];
-    const tag = tags[Math.round(Math.random() * (tags.length - 1))];
-    images.push(url + width + '/' + height + '/' + tag);
+    const tagIndex = Math.round(Math.random() * (tags.length - 1));
+    images.push(url + width + '/' + height + '/' + tags[tagIndex]);
+    tags.splice(tagIndex, 1);
   }
   return images;
 };
 
-for (let i = 1; i <= 100; i++) {
-  let numImages = Math.round(Math.random() * 3) + 1;
+for (let i = 1; i <= products; i++) {
+  let numImages = Math.round(Math.random() * 4) + 1;
   db.models.ProductImages.create({
     productId: i,
     images: generateImages(numImages)
+  }).then(() => {
+    count++;
+    if (count >= products) {
+      console.log('Database Seeded');
+      process.exit();
+    }
   });
 }
