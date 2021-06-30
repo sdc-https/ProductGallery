108b0ec68b7d1a256d3fe0313dd45f8be8f4f6a6
Christopher Raffaele <alumnye@gmail.com>
2021-06-30 11:04:03 -0400

    seeding script working

--- couchdb/index.js
+++ couchdb/index.js
@@ -5,25 +5,24 @@ const countUrl = `http://admin:${config.pwd}@localhost:5984/images`;
 const url = `http://admin:${config.pwd}@localhost:5984/images/_bulk_docs`;
 
 // generate records
-// const generateData = async () => {
-//   let idCount = 0
-//   for (let i = 0; i < 1000; i++) {
-//     const recordsArr = [];
-//     for (let k = 0; k < 10000; k++) {
-//       idCount++;
-//       const record = {
-//           productId: idCount,
-//           images: ['test data', 'hey fren']
-//       }
-//       recordsArr.push(record);
-//     }
-//     // bulk write 100 records to db
-//     await axios.post(url, {
-//       docs: recordsArr
-//     })
-//   }
-// }
-// generateData();
+const generateData = async () => {
+  let idCount = 0
+  for (let i = 0; i < 1000; i++) {
+    const recordsArr = [];
+    for (let k = 0; k < 10000; k++) {
+      idCount++;
+      const record = {
+          productId: idCount,
+          images: ['test data', 'hey fren']
+      }
+      recordsArr.push(record);
+    }
+    await axios.post(url, {
+      docs: recordsArr
+    })
+  }
+}
+generateData();
 
 // count documents
 axios.get(countUrl)
