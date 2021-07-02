798f88b708753cfd3489d87b49326bcf923a69c2
Christopher Raffaele <alumnye@gmail.com>
2021-07-02 11:27:06 -0400

    amended fetch script to retrieve 1000 unique images

--- images/fetchImages.js
+++ images/fetchImages.js
@@ -1,16 +1,21 @@
 const download = require('image-downloader');
 const axios = require('axios');
 const fs = require('fs');
-const url = 'https://loremflickr.com/320/240/dogs';
-
-const options = {
-  url: 'https://loremflickr.com/320/240',
-  dest: '/Users/owner/sdc/ProductGallery/images/repository'
-}
+const url = 'https://loremflickr.com/';
+const sizes = [768, 1280, 900];
+const tags = ['tech', 'animals', 'arch', 'people', 'nature', 'dogs', 'paris'];
+const images = [];
 
 const fetchImages = async (n) => {
+  var id = 0;
   for (let i = 0; i < n; i++) {
-    await axios.get(url, {responseType: 'stream'})
+    id++;
+    const width = sizes[Math.round(Math.random() * (sizes.length - 1))];
+    const height = sizes[Math.round(Math.random() * (sizes.length - 1))];
+    const tagIndex = Math.round(Math.random() * (tags.length - 1));
+    await axios.get(url + width + '/' + height + '/' + tags[tagIndex] + '?lock=' + id, {
+      responseType: 'stream'
+    })
     .then(async (res) => {
       // image-downloader call
       await download.image({
@@ -26,5 +31,5 @@ const fetchImages = async (n) => {
         })
   }
 }
-fetchImages(1000);
+fetchImages(900);
 
