68428f89f262a9fd02da0bfef571bf2088135e7b
Christopher Raffaele <alumnye@gmail.com>
2021-07-03 18:02:49 -0400

    created read function for couchddb

--- couchdb/index.js
+++ couchdb/index.js
@@ -2,6 +2,7 @@ const axios = require('axios');
 const config = require('../config.js');
 const countUrl = `http://${config.username}:${config.pwd}@localhost:5984/${config.database}`;
 const postUrl = `http://${config.username}:${config.pwd}@localhost:5984/${config.database}/_bulk_docs`;
+const findOneUrl = `http://${config.username}:${config.pwd}@localhost:5984/${config.database}/`;
 
 // generate records
 const generateData = async () => {
@@ -10,21 +11,33 @@ const generateData = async () => {
     const recordsArr = [];
     for (let k = 0; k < 10000; k++) {
       idCount++;
-      const record = {
-          productId: idCount,
-          images: ['test data', 'hey fren']
+      // const imageCount = Math.floor( Math.random() * )
+      for (let j = 0; j < imageCount; j++) {
+        const record = {
+            _id: idCount.toString(),
+            images: ['test data', 'hey fren']
+        }
+        recordsArr.push(record);
       }
-      recordsArr.push(record);
     }
     await axios.post(postUrl, {
       docs: recordsArr
     })
+      .catch((err) => {
+        console.error(err);
+      })
   }
 }
 generateData();
 
-// count documents
-// axios.get(countUrl)
-//   .then((res) => {
-//     console.log('doc count:', res.data.doc_count);
-//   })
+//findOne
+const findOne = (id) => {
+  axios.get(findOneUrl + `${id}`)
+    .then((res) => {
+      console.log('success:', res.data);
+    })
+    .catch((err) => {
+      console.error(err);
+    })
+}
+// findOne(101);
