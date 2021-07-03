05da40f59ec713bbc7a8907a047b85af1b9273ba
Christopher Raffaele <alumnye@gmail.com>
2021-07-03 18:09:54 -0400

    switch commit

--- couchdb/index.js
+++ couchdb/index.js
@@ -7,18 +7,22 @@ const findOneUrl = `http://${config.username}:${config.pwd}@localhost:5984/${con
 // generate records
 const generateData = async () => {
   let idCount = 0
-  for (let i = 0; i < 1000; i++) {
+  for (let i = 0; i < 10; i++) {
     const recordsArr = [];
-    for (let k = 0; k < 10000; k++) {
+    for (let k = 0; k < 100; k++) {
       idCount++;
       // const imageCount = Math.floor( Math.random() * )
-      for (let j = 0; j < imageCount; j++) {
-        const record = {
-            _id: idCount.toString(),
-            images: ['test data', 'hey fren']
-        }
-        recordsArr.push(record);
+      const record = {
+        _id: idCount.toString(),
+        images: []
       }
+      const imageCount = 3;
+      for (let j = 0; j < imageCount; j++) {
+        const imageId = Math.floor( (Math.random() * 1000) + 1 );
+        const image = `https://sdc-http-images.s3.amazonaws.com/sdc-image-${imageId}.jpg`;
+        record.images.push(image);
+      }
+      recordsArr.push(record);
     }
     await axios.post(postUrl, {
       docs: recordsArr
